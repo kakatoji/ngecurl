3d4dff2a7f26e44299a41e5064d076bbcb497c78
kakatoji <kakatoji0@gmail.com>
2021-06-15 14:17:47 +0800

    me.php

--- me.php
+++ me.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(0);
 function curl($url, $post = 0, $httpheader = 0, $proxy = 0){ // url, postdata, http headers, proxy, uagent
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -31,13 +31,28 @@ function curl($url, $post = 0, $httpheader = 0, $proxy = 0){ // url, postdata, h
             return array($header, $body);
         }
     }
-  function array_to_cookies($source){
-        if(!is_array($source)){
-            return "NOT ARRAY!";
-        }else{
-            return str_replace(array('\n', '\\', '{"', '"}', '":"', '","'), array('', '', '', '', '=', '; '), json_encode($source));
-        }
-    }
+  function pad(){
+    $s=shell_exec('tput cols');
+    $r=mb_convert_encoding('&#x2591;', 'UTF-8', 'HTML-ENTITIES');
+    $z=str_repeat($r,$s);
+    $_z=str_repeat("~",$s);
+    echo col($_z,"m").PHP_EOL;
+    echo $z.PHP_EOL;
+    echo col($_z,"m").PHP_EOL;
+  }
+  function ban($z){
+    $_a="kakatoji";
+    $_b=mb_convert_encoding('&#x2591;', 'UTF-8', 'HTML-ENTITIES');
+    $_c=str_repeat($_b,10);
+    pad();
+    echo $_c.col(strtoupper(" Author: "),"k").col(strtoupper($_a),"c").PHP_EOL;
+    echo $_c.col(strtoupper(" scriptBot: "),"k").col(strtoupper($z),"c").PHP_EOL;
+    pad();
+  }
+  function cxl($in,$text){
+  $_a="[";$a_="]";
+  echo col($_a,"p").col($in,"m").col($a_,"p").col($text,"u").PHP_EOL;
+}
   function name(){
     $url="https://randomuser.me/api/?format=json";
     $data=json_decode(file_get_contents($url),1);
@@ -61,4 +76,4 @@ function curl($url, $post = 0, $httpheader = 0, $proxy = 0){ // url, postdata, h
     file_put_contents($data,json_encode($arr,JSON_PRETTY_PRINT));
   }
   function col($str,$color){
-$warna=array('x'=>"\033[0m",'p'=>"\033[1;0m",'a'=>"\033[1;30m",'m'=>"\033[1;31m",'h'=>"\033[1;32m",'k'=>"\033[1;33m",'b'=>"\033[1;34m",'u'=>"\033[1;35m",'c'=>"\033[1;36m",'px'=>"\033[1;7m",'mp'=>"\033[1;41m",'hp'=>"\033[1;42m",'kp'=>"\033[1;43m",'bp'=>"\033[1;44m",'up'=>"\033[1;45m",'cp'=>"\033[1;46m",'pp'=>"\033[1;47m",'ap'=>"\033[1;100m",'pm'=>"\033[7;41m",);return $warna[$color].$str."\033[0m";}
+$warna=array('x'=>"\033[0m",'p'=>"\033[1;37m",'a'=>"\033[1;30m",'m'=>"\033[1;31m",'h'=>"\033[1;32m",'k'=>"\033[1;33m",'b'=>"\033[1;34m",'u'=>"\033[1;35m",'c'=>"\033[1;36m",'px'=>"\033[1;7m",'mp'=>"\033[1;41m",'hp'=>"\033[1;42m",'kp'=>"\033[1;43m",'bp'=>"\033[1;44m",'up'=>"\033[1;45m",'cp'=>"\033[1;46m",'pp'=>"\033[1;47m",'ap'=>"\033[1;100m",'pm'=>"\033[7;41m",);return $warna[$color].$str."\033[0m";}
