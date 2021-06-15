293edcab7c3a645c5aa6a99efe10e3c1dc49c4b5
kakatoji <kakatoji0@gmail.com>
2021-06-15 10:03:01 +0800

    me.php

--- me.php
+++ me.php
@@ -60,3 +60,5 @@ function curl($url, $post = 0, $httpheader = 0, $proxy = 0){ // url, postdata, h
     $arr=array_merge($json,$data_post);
     file_put_contents($data,json_encode($arr,JSON_PRETTY_PRINT));
   }
+  function col($str,$color){
+$warna=array('x'=>"\033[0m",'p'=>"\033[1;0m",'a'=>"\033[1;30m",'m'=>"\033[1;31m",'h'=>"\033[1;32m",'k'=>"\033[1;33m",'b'=>"\033[1;34m",'u'=>"\033[1;35m",'c'=>"\033[1;36m",'px'=>"\033[1;7m",'mp'=>"\033[1;41m",'hp'=>"\033[1;42m",'kp'=>"\033[1;43m",'bp'=>"\033[1;44m",'up'=>"\033[1;45m",'cp'=>"\033[1;46m",'pp'=>"\033[1;47m",'ap'=>"\033[1;100m",'pm'=>"\033[7;41m",);return $warna[$color].$str."\033[0m";}
