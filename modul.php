780e7d19cc293a8f4a104a0654099cef09632675
Aang saputra <kakatoji0@gmail.com>
2022-01-27 12:32:30 +0800

    Update modul.php

--- modul.php
+++ modul.php
@@ -81,6 +81,15 @@ class Modul{
         if($i >= count ($wrn)){$i=0;}
         endwhile;
     }
+    public function gen_uuid() {
+        return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+            mt_rand( 0, 0xffff ),
+            mt_rand( 0, 0x0fff ) | 0x4000,
+            mt_rand( 0, 0x3fff ) | 0x8000,
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+        );
+    }
     public function save($data,$data_post){
         if(!file_get_contents($data)){
           file_put_contents($data,"[]");}
