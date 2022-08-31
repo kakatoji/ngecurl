d4e17e4044cf057a563df286603103bd88405d94
Aang saputra <kakatoji0@gmail.com>
2022-09-01 00:05:17 +0800

    Update modul.php

--- modul.php
+++ modul.php
@@ -90,6 +90,52 @@ class Modul{
         $result=json_decode($this->curl($url,$data,$ua)[1],1);
         return $result["result"]["texts"][0]["text"];
     }
+    public function acssi_calvin($string){ 
+           $string = preg_replace("/[^a-zA-Z\ ]/","",$string); 
+           $acssi = [ 
+                    "a" => ["┌─┐","├─┤","┴ ┴"], 
+                    "b" => ["┌┐ ","├┴┐","└─┘"], 
+                    "c" => ["┌─┐","│  ","└─┘"], 
+                    "d" => ["┌┬┐"," ││","─┴┘"], 
+  "e" => ["┌─┐","├┤ ","└─┘"], 
+  "f" => ["┌─┐","├┤ ","└  "], 
+  "g" => ["┌─┐","│ ┬","└─┘"], 
+  "h" => ["┬ ┬","├─┤","┴ ┴"], 
+  "i" => ["┬","│","┴"], 
+  "j" => [" ┬"," │","└┘"], 
+  "k" => ["┬┌─","├┴┐","┴ ┴"], 
+  "l" => ["┬  ","│  ","┴─┘"], 
+  "m" => ["┌┬┐","│││","┴ ┴"], 
+  "n" => ["┌┐┌","│││","┘└┘"], 
+  "o" => ["┌─┐","│ │","└─┘"], 
+  "p" => ["┌─┐","├─┘","┴  "], 
+  "q" => ["┌─┐ ","│─┼┐","└─┘└"], 
+  "r" => ["┬─┐","├┬┘","┴└─"], 
+  "s" => ["┌─┐","└─┐","└─┘"], 
+  "t" => ["┌┬┐"," │ "," ┴ "], 
+  "u" => ["┬ ┬","│ │","└─┘"], 
+  "v" => ["┬  ┬","└┐┌┘"," └┘ "], 
+  "w" => ["┬ ┬","│││","└┴┘"], 
+  "x" => ["─┐ ┬","┌┴┬┘","┴ └─"], 
+  "y" => ["┬ ┬","└┬┘"," ┴ "], 
+  "z" => ["┌─┐","┌─┘","└─┘"], 
+  " " => [" "," "," "] 
+ ]; 
+ $x = str_split($string);
+ print_r($acssi);exit;
+ foreach($x as $data){ 
+  print c.$acssi[$data][0];
+ } 
+ print "\n"; 
+ foreach($x as $data){ 
+  print p.$acssi[$data][1]; 
+ } 
+ print "\n"; 
+ foreach($x as $data){ 
+  print m.$acssi[$data][2]; 
+ } 
+ print "\n"; 
+} 
     public function col($str,$color){
         $warna=array('x'=>"\033[0m",'p'=>"\033[1;37m",'a'=>"\033[1;30m",'m'=>"\033[1;31m",'h'=>"\033[1;32m",'k'=>"\033[1;33m",'b'=>"\033[1;34m",'u'=>"\033[1;35m",'c'=>"\033[1;36m",'px'=>"\033[1;7m",'mp'=>"\033[1;41m",'hp'=>"\033[1;42m",'kp'=>"\033[1;43m",'bp'=>"\033[1;44m",'up'=>"\033[1;45m",'cp'=>"\033[1;46m",'pp'=>"\033[1;47m",'ap'=>"\033[1;100m",'pm'=>"\033[7;41m",);return $warna[$color].$str."\033[0m";}
     public function timer($text,$timer){
@@ -125,6 +171,12 @@ class Modul{
             mt_rand( 0, 0x3fff ) | 0x8000,
             mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
         );
+    }
+    public function tput(){
+          if(!$a=shell_exec("which tput")){
+              echo "install dulu tput".PHP_EOL;
+              system("pkg install ncurses-utils");
+           }
     }
     public function save($data,$data_post){
         if(!file_get_contents($data)){
